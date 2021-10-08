40f5d746c399dad74e8ec9bb5bdab9f948f25699
Rusching <2785442070@qq.com>
2021-10-08 22:58:56 +0800

    add antialiasing

--- common_header.h
+++ common_header.h
@@ -4,6 +4,7 @@
 #include<cmath>
 #include<memory>
 #include<limits>
+#include<cstdlib>
 
 #include "ray.h"
 #include "vec3.h"
@@ -11,11 +12,37 @@
 using std::shared_ptr;
 using std::make_shared;
 
+/* I think it's unnessesary to use 'using sqrt' 
+because in header "vec3.h", it actually has already used 'sqrt'.
+Later debug this by commenting the line below to see if there will
+arouse any problems.
+ */
+
+using std::sqrt;
+
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 inline double degree2radian (const double degree) {
     return degree * pi / 180.0;
+}
+
+inline double random_double() {
+    // [0, 1)
+    return rand() / (RAND_MAX +1.0);
+}
+
+inline double random_double(double min, double max) {
+    // [min, max)
+    return (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min)
+        return min;
+    if (x > max) 
+        return max;
+    return x;
 }
 
 #endif
