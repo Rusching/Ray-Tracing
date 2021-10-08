bce3d1d8272a0ecaab5768d027a7fbd9cee47ae6
Rusching <2785442070@qq.com>
2021-10-08 17:30:39 +0800

    render sphere

--- color.h
+++ color.h
@@ -4,11 +4,12 @@
 #include "vec3.h"
 #include <iostream>
 
-void write_color(std::ostream &out, color pixel_color) {
+void write_color(std::ostream &out, color pixel_color)
+{
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' ' 
-        << static_cast<int>(255.999 * pixel_color.y()) << ' ' 
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n'; 
-} 
+    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+        << static_cast<int>(255.999 * pixel_color.y()) << ' '
+        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
 
 #endif
